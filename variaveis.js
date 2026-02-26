c92ec40c8f5829a41cc6d08464936d6193cb2cb3
maria-off <maria.alves.pereira18@escola.pr.gov.br>
2026-02-26 09:48:45 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,43 @@
+let a = 40;
+let b = 10;
+let c = 20;
+let total = 0;
+let totalde = 0;
+let x = 5;
+total = (a + b + c);
+totalde =  (a + b + c) /x
+console.log("O valor do total é:", total);
+console.log("O valor do total é:", totalde);
+console.log( outraconta());
+console.log(media());
+
+// inicio da function
+function outraconta() {
+    var d = 10;
+    var e = 20;
+    var f = 30;
+    var tot = 0;
+    var totde = 0;
+    var xx = 100;
+    tot = ( d + e + f);
+    totde = ( d + e + f )/ xx
+    //console.log("O valor de tot na function é:", tot);
+    //console.log("O valor de totde na function é: ",totde);
+    return tot;
+    return totde;
+
+} 
+
+
+
+
+function media(){
+    let nfinal = 0;
+    n1 = 30;
+    n2 = 90;
+    n3 = 100;
+    nfinal = (n1 + n2 + n3);
+    nfinal = (n1 + n2 + n3)/ 3;
+    //console.log("A media do aluno é :", nfinal.toFixed(1));
+    return nfinal.toFixed(1);
+}
