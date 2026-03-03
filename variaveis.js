0fffb67900d796b68da2e9994439de29c8c45f04
maria-off <maria.alves.pereira18@escola.pr.gov.br>
2026-03-03 09:32:11 -0300

    Update variaveis.js

--- variaveis.js
+++ variaveis.js
@@ -10,6 +10,9 @@ console.log("O valor do total é:", total);
 console.log("O valor do total é:", totalde);
 console.log( outraconta());
 console.log(media());
+console.log(carros());
+console.log(triangulo());
+console.log(frutas())
 
 // inicio da function
 function outraconta() {
@@ -40,4 +43,47 @@ function media(){
     nfinal = (n1 + n2 + n3)/ 3;
     //console.log("A media do aluno é :", nfinal.toFixed(1));
     return nfinal.toFixed(1);
+}
+
+function carros () {
+   carros1 = "fusca";
+   km = 100;
+   p = 12,79;
+
+   carros2 = "celta";
+   km2 = 360;
+   p2 = 45,90;
+   console.log("o total do fusca é:",km*p);
+   console.log("o total  do celta é:",km2*p2);
+}
+
+function triangulo(){
+    b = 5;
+    a = 12;
+    t =b*a/2;
+    console.log("O valor da área é:", t);
+}
+
+function frutas(){
+    let frutas=[ "Maça", "Banana","Laranja"];
+    console.log(frutas.length);
+    console.log(frutas);
+    frutas.push('Manga');
+    console.log(frutas);
+    console.log(frutas.length);
+    frutas.pop();
+    frutas.pop();
+    console.log(frutas);
+    frutas.pop();
+    frutas.pop();
+    console.log(frutas);
+    frutas.push("Uva");
+    frutas.push("Amora");
+    frutas.push("Mexirica" ,"Abacate", "Abacaxi")
+    console.log(frutas);
+    console.log(" A frutas na posição 1 é:", frutas[1]);
+    console.log(frutas.length);
+    console.log("O total de registros é de",frutas.length + 1, "Frutas");
+    frutas.unshift("Cereija");
+    console.log(frutas);
 }
